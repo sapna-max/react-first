bdc82471dcc18c283abbcbebcd38d31d59d3c9bb
Sapna Developer <78731492+sapna-max@users.noreply.github.com>
2022-08-09 19:23:35 +0530

    Update http.js

--- http.js
+++ http.js
@@ -1,8 +1,7 @@
 var http = require('http');
+const port = process.env.PORT||8210;
 
 var server = http.createServer(function(req,res){
     res.write("Welcome to nodeJs");
     res.end()
 })
-
-server.listen(9800)
